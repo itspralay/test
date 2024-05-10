e3c3fae057dcd7ed4dbcdfbae9dfb5b329d3f328
Pralay Pal <pralaypal111@gmail.com>
2024-05-10 14:03:21 +0530

    h

--- main.js
+++ main.js
@@ -1,43 +1,75 @@
 let cvs = document.getElementById("myCanvas");
 let ctx = cvs.getContext("2d");
 
-
-
-
+let w = 40;
+let h = 40;
 let i = 0;
-let j=0;
+let j = 0;
+
+let x = 1; 
+let y = 0;
+
 
 
 function draw() {
-    ctx.clearRect(0, 0, 300, 150)
-    i += 10;
-    if (i > 300) {
-        i = 0;
-    }
-    j += 10;
-    if (j> 150){
-        j = 0;
-    }
+  ctx.clearRect(0, 0, 300, 150);
+  i += x;
+  j += y;
 
-    ctx.fillStyle = "red";
-    ctx.fillRect(i, 50, 50, 50);
+  if (i > 300) i = 0;
+  if (j > 150) j = 0;
+  if (i < 0) i = 300;
+  if (j < 0) j = 150;
 
-    ctx.fillStyle = "green"
-    ctx.fillRect(i, j, 50, 50);
+  ctx.fillStyle = "red";
+  ctx.fillRect(i, j, w, h);
 
-    ctx.fillStyle = "blue"
-    ctx.beginPath();
-    ctx.arc(100, 100, 25, 0, Math.PI*2, false);
-    ctx.fill()
-    ctx.closePath();
 }
+
+addEventListener("keydown", function(event) {
+    console.log(event.keyCode);
+    if (event.keyCode == 40) {
+        x = 0;
+        y = 1;
+    }
+
+    if (event.keyCode == 37) {
+        x = -1;
+        y = 0;
+    }
+    if (event.keyCode == 38) {
+        x = 0;
+        y = -1;
+    }
+    if (event.keyCode == 39) {
+        x = 1
+        y = 0;
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function loop() {
-    draw();
+  draw();
 
-
-  requestAnimationFrame(loop);  
+  requestAnimationFrame(loop);
 }
 loop();
-
